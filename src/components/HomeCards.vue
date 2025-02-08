d0995a118fdcd5e849ca2bbb85ebfaa6a44c79ec
janaka120 <janakastill@gmail.com>
2025-02-08 21:38:42 +0800

    Remove unused code

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -25,31 +25,6 @@ import Card from '@/components/Card.vue';
                         Add Job
                     </RouterLink>
                 </Card>
-                <!-- <div class="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold">For Developers</h2>
-            <p class="mt-2 mb-4">
-              Browse our Vue jobs and start your career today
-            </p>
-            <a
-              href="jobs.html"
-              class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Jobs
-            </a>
-          </div> -->
-
-                <!-- <div class="bg-green-100 p-6 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold">For Employers</h2>
-            <p class="mt-2 mb-4">
-              List your job to find the perfect developer for the role
-            </p>
-            <a
-              href="add-job.html"
-              class="inline-block bg-green-500 text-white rounded-lg px-4 py-2 hover:bg-green-600"
-            >
-              Add Job
-            </a>
-          </div> -->
             </div>
         </div>
     </section>
